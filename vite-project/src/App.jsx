4774b5a696e8797719cdd04759f6cb93df3ef640
oliv3r1 <oliver.suomela@metropolia.fi>
2023-04-24 13:59:53 +0300

    settings sivu tehty ja liitetty layouttiin

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,6 +10,7 @@ import Logout from './views/Logout';
 import Upload from './views/Upload';
 import MyFiles from './views/MyFiles';
 import Update from './views/Update';
+import Settings from './views/Settings';
 
 console.log('base', import.meta.env.BASE_URL);
 
@@ -26,6 +27,7 @@ const App = () => {
             <Route path="/upload" element={<Upload />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/myfiles" element={<MyFiles />} />
+            <Route path="/settings" element={<Settings />} />
             <Route path="/update" element={<Update />} />
           </Route>
         </Routes>
---
+++ vite-project/src/views/Settings.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+
+
+const Settings = () => {
+  return (
+    <>
+      <Typography component="h1" variant="h3">
+        Settings
+      </Typography>
+
+    </>
+  );
+};
+
+export default Settings;
