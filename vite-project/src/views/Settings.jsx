471317d5f00c9e9e3eb65ccd76617a4ca172f47c
oliv3r1 <oliver.suomela@metropolia.fi>
2023-04-24 14:19:42 +0300

    logout nappi toimimaan

--- vite-project/src/views/Settings.jsx
+++ vite-project/src/views/Settings.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import {Typography} from '@mui/material';
-
+import React, {useState} from 'react';
+import {Button, Stack, Typography} from '@mui/material';
+import Logout from './Logout';
 
 const Settings = () => {
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoggedOut(true);
+  };
+
   return (
     <>
-      <Typography component="h1" variant="h3">
-        Settings
-      </Typography>
-
+      {isLoggedOut ? (
+        <Logout />
+      ) : (
+        <>
+          <Typography component="h1" variant="h3">
+            Settings
+          </Typography>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Button>Edit Profile</Button>
+            <Button>Info</Button>
+            <Button onClick={handleLogout}>Logout</Button>
+          </Stack>
+        </>
+      )}
     </>
   );
 };
