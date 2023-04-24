373768251ea9e7b90d4c302963bef0c557b23beb
oliv3r1 <oliver.suomela@metropolia.fi>
2023-04-24 14:33:26 +0300

    info buttoni

--- vite-project/src/views/Settings.jsx
+++ vite-project/src/views/Settings.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
-import {Button, Stack, Typography} from '@mui/material';
+import {Button, Stack, Typography, colors} from '@mui/material';
 import Logout from './Logout';
 
 const Settings = () => {
   const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const [isInfoClicked, setIsInfoClicked] = useState(false);
 
+  const handleInfoClick = () => {
+    setIsInfoClicked(!isInfoClicked);
+  };
   const handleLogout = () => {
     setIsLoggedOut(true);
   };
@@ -19,14 +23,20 @@ const Settings = () => {
             Settings
           </Typography>
           <Stack
+
             direction="column"
             justifyContent="center"
             alignItems="center"
             spacing={2}
           >
-            <Button>Edit Profile</Button>
-            <Button>Info</Button>
-            <Button onClick={handleLogout}>Logout</Button>
+            <Button style={{ fontSize: '1.5rem' }}>Edit Profile</Button>
+            <Button style={{ fontSize: '1.5rem' }} onClick={handleInfoClick}>Info</Button>
+        {isInfoClicked && (
+          <div style={{fontSize: '1.2rem', padding: '1rem'}}>
+            Tässä meidän mahtava applikaatio!
+          </div>
+        )}
+            <Button style={{ fontSize: '1.5rem', }} onClick={handleLogout}>Logout</Button>
           </Stack>
         </>
       )}
