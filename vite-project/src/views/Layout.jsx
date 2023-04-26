ee86c9db08e1c5e95fb5bb74460af7aa7a44282d
sclash <samuelpellikka@gmail.com>
2023-04-26 18:28:58 +0300

    korjauksia bottombariin

--- vite-project/src/views/Layout.jsx
+++ vite-project/src/views/Layout.jsx
@@ -27,10 +27,6 @@ import {
 
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Layout = () => {
   const {user, setUser} = useContext(MediaContext);
