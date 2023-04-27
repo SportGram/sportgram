c5adbac782e1431a1c808b60da5d990c0b5b6771
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-04-27 14:24:22 +0300

    search

--- vite-project/src/components/MediaTable.jsx
+++ vite-project/src/components/MediaTable.jsx
@@ -1,19 +1,32 @@
-import {ImageList} from '@mui/material';
+import { useState } from 'react';
+import { ImageList, TextField } from '@mui/material';
 import PropTypes from 'prop-types';
-import {useMedia} from '../hooks/ApiHooks';
-import {useWindowSize} from '../hooks/WindowHooks';
+import { useMedia } from '../hooks/ApiHooks';
+import { useWindowSize } from '../hooks/WindowHooks';
 import MediaRow from './MediaRow';
 
-const MediaTable = ({myFilesOnly = false}) => {
-  const {mediaArray, deleteMedia} = useMedia(myFilesOnly);
+const MediaTable = ({ myFilesOnly = false }) => {
+  const { mediaArray, deleteMedia } = useMedia(myFilesOnly);
   const windowSize = useWindowSize();
+  const [searchText, setSearchText] = useState('');
+
+  const filteredMediaArray = mediaArray.filter((item) =>
+    item.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+  };
 
   return (
-    <ImageList cols={windowSize.width > 768 ? 3 : 2} gap={8}>
-      {mediaArray.map((item, index) => {
-        return <MediaRow key={index} file={item} deleteMedia={deleteMedia} />;
-      })}
-    </ImageList>
+    <>
+      <TextField onChange={handleSearch} />
+      <ImageList cols={windowSize.width > 768 ? 3 : 2} gap={8}>
+        {filteredMediaArray.map((item, index) => {
+          return <MediaRow key={index} file={item} deleteMedia={deleteMedia} />;
+        })}
+      </ImageList>
+    </>
   );
 };
 
