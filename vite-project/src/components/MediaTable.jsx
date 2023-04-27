5574434b895f738d26c977297444dfe15f407b00
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-04-27 16:12:14 +0300

    updates

--- vite-project/src/components/MediaTable.jsx
+++ vite-project/src/components/MediaTable.jsx
@@ -1,9 +1,10 @@
 import '../search.css';
 import { useState } from 'react';
-import { ImageList, TextField } from '@mui/material';
+import { ImageList, TextField, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useMedia } from '../hooks/ApiHooks';
 import { useWindowSize } from '../hooks/WindowHooks';
+import gif from '../assets/noresults.gif'
 import MediaRow from './MediaRow';
 
 const MediaTable = ({ myFilesOnly = false }) => {
@@ -19,16 +20,21 @@ const MediaTable = ({ myFilesOnly = false }) => {
     setSearchText(e.target.value);
   };
 
-
-
   return (
     <>
       <TextField className='search' variant="standard" label="Search posts..." onChange={handleSearch} />
-      <ImageList cols={1} gap={8} sx={{ maxWidth: 500 }}>
-        {filteredMediaArray.map((item, index) => {
-          return <MediaRow key={index} file={item} deleteMedia={deleteMedia} />;
-        })}
-      </ImageList>
+      {filteredMediaArray.length === 0 ? (
+        <>
+          <Typography variant="h6">No search results found. Go upload something</Typography>
+          <img src={gif}></img>
+        </>
+      ) : (
+        <ImageList cols={1} gap={8} sx={{ maxWidth: 500 }}>
+          {filteredMediaArray.map((item, index) => {
+            return <MediaRow key={index} file={item} deleteMedia={deleteMedia} />;
+          })}
+        </ImageList>
+      )}
     </>
   );
 };
